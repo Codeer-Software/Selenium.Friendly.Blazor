045aa76847fabff4b9447c4512de1f5803117b14
Ishikawa-Tatsuya <stone.guitar.777@gmail.com>
2022-02-23 14:50:59 +0900

    Adjust Test

--- Project/SeleniumTest/SeleniumTest/UnitTest1.cs
+++ Project/SeleniumTest/SeleniumTest/UnitTest1.cs
@@ -3,12 +3,12 @@ using Friendly.Blazor.Inside.Protocol;
 using Newtonsoft.Json;
 using NUnit.Framework;
 using OpenQA.Selenium.Chrome;
+using System.Threading;
 
 namespace SeleniumTest
 {
     public class Tests
     {
-
         ChromeDriver? _driver;
 
         [SetUp]
@@ -21,26 +21,25 @@ namespace SeleniumTest
         public void Test1()
         {
             _driver!.Url = "https://localhost:7128/counter";
-            var app = new BlazorAppFriend(_driver);
-            string test = app.Type("BlazorApp.Sample1").Test();
 
+            while(_driver.Title != "Counter") Thread.Sleep(100);
+
+
+            var app = new BlazorAppFriend(_driver);
+
+            string test = app.Type("BlazorApp.Sample1").Test();
             int test2 = app.Type("BlazorApp.Sample1").Test2();
             int test3 = app.Type("BlazorApp.Sample1").Test3;
             app.Type("BlazorApp.Sample1").Test3 = 10;
-
-
             var sample2 = app.Type("BlazorApp.Sample1").Sample2;
             sample2.Value = 100;
 
+            //カウンター画面取得
             var counter = app.FindComponentByType("BlazorApp.Pages.Counter");
 
-           // var counter = app.Type("BlazorApp.Pages.Counter")._this;
-
-            var x = ((AppVar)counter).IsNull;
-
+            //操作
             counter.currentCount = 1000;
             counter.StateHasChanged();
-
         }
     }
 }
